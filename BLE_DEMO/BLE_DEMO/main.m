ce8d09a44d6d57948c4bf6b176d198c8f7b9c63e
a87835546 <87835546@qq.com>
2017-09-25 18:09:15 +0800

    1.0
    create new demo

---
+++ BLE_DEMO/BLE_DEMO/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BLE_DEMO
+//
+//  Created by eason on 2017/9/25.
+//  Copyright © 2017年 carey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
